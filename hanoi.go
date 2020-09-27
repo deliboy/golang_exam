114660253ca04de84033808336fcc39450fcbfa4
= <jindalre@gmail.com>
2020-09-27 20:03:07 +0900

    로직구현

--- hanoi.go
+++ hanoi.go
@@ -1,1 +1,23 @@
 package main
+
+import "fmt"
+
+func Move(n int, from, to, via string) {
+
+	if n <= 0 {
+		return
+	}
+
+	Move(n-1, from, via, to)
+	fmt.Println(from, "->", to)
+	Move(n-1, via, to, from)
+}
+
+func Hanoi(n int) {
+	fmt.Println("Number of diks:", n)
+	Move(n, "From", "To", "Via")
+}
+
+func main() {
+	Hanoi(3)
+}
